50c80c2265b6fa544ddf93038943f7b5c24f9a5c
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2025-04-11 15:24:36 -0400

    add api keys to .env file

--- bugzilla/bugzilla.py
+++ bugzilla/bugzilla.py
@@ -1,8 +1,12 @@
 import requests
 import datetime
+from dotenv import load_dotenv
+import os
 
 # Bugzilla API key
-API_KEY = "sicSwVcOxOwvLCn12804YSB0nhlCuSBw4FkhKoKK"
+load_dotenv(dotenv_path="../secrets/.env")
+
+API_KEY = os.getenv("BUGZILLA_API_KEY")
 
 # Bugzilla API endpoint
 BUGZILLA_API_URL = "https://bugzilla.mozilla.org/rest/bug"
