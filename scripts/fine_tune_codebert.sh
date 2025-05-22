b3244e07f816149f7415f1cd50858c2b80305bf3
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2025-05-21 22:26:36 -0400

    creat new script for codebert

---
+++ scripts/fine_tune_codebert.sh
@@ -0,0 +1,22 @@
+#!/encs/bin/tcsh
+
+#SBATCH --job-name=llm-finetune
+#SBATCH --output=/speed-scratch/a_s87063/repos/perf-pilot/slurm_jobs/%x-%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --partition=pg
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --time=15:00:00
+#SBATCH --account=pcr
+#SBATCH --constraint=gpu32
+
+echo "Preparing training environment"
+source /speed-scratch/$USER/repos/perf-pilot/scripts/train_prepare.csh
+
+echo "running the training script"
+python python sequence_classification.py --model_name codebert-base --live_metrics --perf_data
+echo "training finished"
+
+echo "Cleaning up"
+source /speed-scratch/$USER/repos/perf-pilot/scripts/train_cleanup.csh
