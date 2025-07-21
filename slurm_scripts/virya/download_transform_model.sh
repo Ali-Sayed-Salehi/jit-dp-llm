149a128d4c5014a2a9d1e892576f500577711b30
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2025-07-20 22:28:38 -0400

    fix bugs

--- slurm_scripts/virya/download_transform_model.sh
+++ slurm_scripts/virya/download_transform_model.sh
@@ -16,4 +16,4 @@ python /home/a_s87063/repos/perf-pilot/llama/download_model.py --model_id meta-l
 # python /home/a_s87063/repos/perf-pilot/llama/download_model.py --model_id syzymon/long_llama_code_7b --model_head sequence-classification
 # python /home/a_s87063/repos/perf-pilot/llama/attach_classification_head.py --base_lm_path /home/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-3.1-8B --save_path /home/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification/custom
 # python /home/a_s87063/repos/perf-pilot/llama/attach_classification_head_llama4.py --base_lm_path /home/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-4-Scout-17B-16E --save_path /home/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification/custom
-echo "training finished"
+echo "download/transform finished"
