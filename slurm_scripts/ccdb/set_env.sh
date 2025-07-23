27af093f5d39f273b244c9a59861b19edbbca390
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2025-07-22 22:26:14 -0400

    add scripts for ccdb

---
+++ slurm_scripts/ccdb/set_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Setting environment variables"
+
+export HF_HOME="/home/$USER/scratch/.cache/huggingface/hf-home"
+export HF_DATASETS_CACHE="/home/$USER/scratch/.cache/huggingface/hf-datasets"
+export PIP_CACHE_DIR="/home/$USER/scratch/.cache/pip/pip-cache"
+
+echo "HF_HOME: ${HF_HOME}"
+echo "PIP_CACHE_DIR: ${PIP_CACHE_DIR}"
