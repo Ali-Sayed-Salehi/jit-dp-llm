1cb0b62be7e35f7e2d6d036298ee35c092c98d3b
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2025-08-06 23:16:26 -0400

    fix bugs with attaching classification head

--- slurm_scripts/speed/download_transform_model.sh
+++ slurm_scripts/speed/download_transform_model.sh
@@ -14,9 +14,8 @@ echo "Preparing training environment"
 source /speed-scratch/a_s87063/repos/perf-pilot/slurm_scripts/speed/train_prepare.csh
 
 echo "Running the download/transform script ..."
-# python /speed-scratch/a_s87063/repos/perf-pilot/llama/download_model.py --model_id meta-llama/Llama-4-Scout-17B-16E --model_head causal-lm
+# python /speed-scratch/a_s87063/repos/perf-pilot/llama/download_model.py --model_id meta-llama/Llama-3.1-70B --model_head sequence-classification
 # python /speed-scratch/a_s87063/repos/perf-pilot/llama/download_model.py --model_id syzymon/long_llama_code_7b --model_head sequence-classification
-# python /speed-scratch/a_s87063/repos/perf-pilot/llama/attach_classification_head.py --base_lm_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-3.1-8B --save_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification/custom
-python /speed-scratch/a_s87063/repos/perf-pilot/llama/attach_classification_head_llama4.py --base_lm_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-4-Scout-17B-16E --save_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification
-accelerate launch --config_file=/speed-scratch/a_s87063/repos/perf-pilot/llama/configs/accelerate_config_deepspeed.yaml --num_machines=$SLURM_NNODES --machine_rank=$SLURM_NODEID /speed-scratch/a_s87063/repos/perf-pilot/llama/attach_classification_head_llama4.py --base_lm_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-4-Scout-17B-16E --save_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification
+python /speed-scratch/a_s87063/repos/perf-pilot/llama/attach_classification_head.py --base_lm_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-3.1-70B --save_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification/custom
+# python /speed-scratch/a_s87063/repos/perf-pilot/llama/attach_classification_head.py --llama4 --base_lm_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/causal-lm/meta-llama/Llama-4-Scout-17B-16E --save_path /speed-scratch/a_s87063/repos/perf-pilot/LLMs/pretrained/sequence-classification/custom
 echo "download/transform finished"
