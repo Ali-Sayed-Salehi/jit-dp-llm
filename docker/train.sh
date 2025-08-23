d307c7094dd541e9c42ebb1ed9bd46f49e2dc1ff
Ali Sayedsalehi <a.s.salehi.95@gmail.com>
2025-08-23 02:23:54 -0400

    fix bug

--- docker/train.sh
+++ docker/train.sh
@@ -5,5 +5,5 @@ nvidia-smi
 free -h
 
 echo "running the training script"
-accelerate launch --config_file=llama/configs/accelerate_config_fsdp.yaml llama/train.py --config llama/configs/train_config.yaml
+FSDP_CPU_RAM_EFFICIENT_LOADING=1 accelerate launch --config_file=llama/configs/accelerate_config_fsdp.yaml llama/train.py --config llama/configs/train_config.yaml
 echo "training finished"
